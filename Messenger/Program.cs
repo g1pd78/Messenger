8ee130cfeff4f72cec760ee0e2b6021d510009cb
andrs <andrs@DESKTOP-RC2G0ID>
2021-07-26 20:07:07 +0500

    Добавьте файлы проекта.

---
+++ Messenger/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message();
+            Console.WriteLine("Hey");
+            Console.WriteLine(msg.ToString());
+        }
+    }
+}
---
+++ Messenger/message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger
+{
+    public class Message
+    {
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Message(string userName, string messageText, DateTime timeStam)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStam;
+        }
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string output = String.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+            return output;
+        }
+    }
+}
