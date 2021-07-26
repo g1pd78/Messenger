05bd2273f059e1fcb5f62bdf9e0ffd1f61a28e96
g1pd78 <g1pd78@gmail.com>
2021-07-26 23:54:37 +0500

    getMessage and sendMessage /// post get

---
+++ CoreServer/Controllers/MessengerController.cs
@@ -0,0 +1,46 @@
+﻿using Messenger;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessengerController : ControllerBase
+    {
+
+        static List<Message> ListOfMessages = new List<Message>();
+
+        // GET api/<MessengerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string outputString = "Have no messages!";
+
+            if (id < ListOfMessages.Count && id >= 0)
+                outputString = JsonConvert.SerializeObject(ListOfMessages[id]);
+            Console.WriteLine(String.Format("Message № {0} : {1}", id, outputString));
+
+            return outputString;
+        }
+
+        // POST api/<MessengerController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Message msg)
+        {
+            if(msg == null)
+            {
+                return BadRequest();
+            }
+            ListOfMessages.Add(msg);
+            Console.WriteLine(String.Format("Count: {0}, Text : {1}", ListOfMessages.Count, msg));
+            return new OkResult();
+        }
+    }
+}
--- Messenger/Program.cs
+++ Messenger/Program.cs
@@ -1,14 +1,52 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 
 namespace Messenger
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+
+        private static void GetNewMessage()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while(msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Hey");
-            Console.WriteLine(msg.ToString());
+            //Message msg = new Message();
+            //Console.WriteLine("Hey");
+            //Console.WriteLine(msg.ToString());
+
+            //Message msg = new Message("g1pd78", "Hey", DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+
+            //Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deserializedMsg);
+
+            /*
+             * {"UserName":"g1pd78","MessageText":"Hey","TimeStamp":"2021-07-26T15:18:16.2503617Z"}
+             *  g1pd78 <26.07.2021 15:18:16>: Hey
+             */
+
+            MessageID = 1;
+            Console.WriteLine("Input your name:");
+            UserName = Console.ReadLine();
+            string messageText = ""; ;
+            while(messageText != "Exit")
+            {
+                Message sendMsg = new Message(UserName, messageText, DateTime.Now);
+                API.SendMessage(sendMsg);
+            }
         }
     }
 }
