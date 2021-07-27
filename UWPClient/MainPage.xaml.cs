b6ff6deefad762bfa7b2ef650d7866620f9f0308
g1pd78 <g1pd78@gmail.com>
2021-07-27 21:04:55 +0500

    UWPClient was created!

---
+++ UWPClient/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Messenger;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace UWPClient
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static string Username;
+        private static int MessageID = 0;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+        DispatcherTimer timer;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) }; // 1 секунда
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                MessageLB.Items.Add(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
+        private void SendB_Click(object sender, RoutedEventArgs e)
+        {
+            string Username = UsernameTB.Text;
+            string Message = MessageTB.Text;
+            if(Username.Length > 1 && Message.Length > 1)
+            {
+                Message msg = new Message(Username, Message, DateTime.Now);
+                API.SendMessage(msg);
+            }
+        }
+    }
+}
---
+++ UWPClient/MessengerClientAPI.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Newtonsoft.Json;  
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace Messenger
+{
+    class MessengerClientAPI
+    {
+ 
+        void TestJSON()
+        {
+            Message msg = new Message("g1pd78", "Hey", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+        }
+        public Message GetMessage(int MessageID)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messenger/" + MessageID.ToString());
+            request.Method = "Get";
+            WebResponse response = request.GetResponse();
+            string status = ((HttpWebResponse)response).StatusDescription;
+            Stream DataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(DataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            DataStream.Close();
+            response.Close();
+            if((status.ToLower() == "ok") && (responseFromServer != "No"))
+            {
+                //Console.WriteLine(responseFromServer + "!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responseFromServer);
+                return deserializedMsg;
+            }
+            return null;
+        }
+
+
+        public bool SendMessage(Message msg)
+        {
+            WebRequest request = WebRequest.Create("http://localhost:5000/api/Messenger/");
+            request.Method = "POST";
+            string postData = JsonConvert.SerializeObject(msg);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            return true;
+        }
+
+    }
+}
